a72f07b3fed6efb7aa1dc5925e0c90285ea667fc
TripleJah <orangehedge@protonmail.com>
2022-09-28 13:24:45 -0500

    added functionality to taskbar

--- flutter_todo/lib/screens/home.dart
+++ flutter_todo/lib/screens/home.dart
@@ -12,6 +12,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   final todosList = ToDo.todoList();
+  final _todoController = TextEditingController();
 
   @override 
   Widget build(BuildContext context) {
@@ -79,6 +80,7 @@ class _HomeState extends State<Home> {
                     borderRadius: BorderRadius.circular(10),
                   ),
                   child: TextField(
+                    controller: _todoController,
                     decoration: InputDecoration(
                       hintText: 'Add a new todo item',
                       border: InputBorder.none
@@ -93,7 +95,9 @@ class _HomeState extends State<Home> {
                 ),
                 child: ElevatedButton(
                   child: Text('+', style: TextStyle(fontSize: 40,),),
-                  onPressed: () {},
+                  onPressed: () {
+                    _addToDoItem(_todoController.text);
+                  },
                   style: ElevatedButton.styleFrom(
                     primary: tdBlue,
                     minimumSize: Size(60, 60),
@@ -119,6 +123,18 @@ class _HomeState extends State<Home> {
       todosList.removeWhere((item) => item.id == id);
     });
   }
+
+  void _addToDoItem(String toDo){
+    setState(() {
+      todosList.add(ToDo(
+        id: DateTime.now().millisecondsSinceEpoch.toString(), 
+        todoText: toDo,
+      ));  
+    });
+    _todoController.clear();   
+  }
+
+
 
 Widget searchBox() {
     return Container(
