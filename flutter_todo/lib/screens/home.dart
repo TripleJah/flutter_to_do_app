5a87e7f9654448b2df14fb54c26171fe49cc236a
TripleJah <orangehedge@protonmail.com>
2022-09-28 12:09:45 -0500

    button to add tasks finished

--- flutter_todo/lib/screens/home.dart
+++ flutter_todo/lib/screens/home.dart
@@ -1,49 +1,85 @@
 import 'package:flutter/material.dart';
 import '../constant/colors.dart';
 import '../widgets/todo_item.dart';
+import '../models/todo.dart';
 
 class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+  Home({Key? key}) : super(key: key);
+
+  final todosList = ToDo.todoList();
 
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: tdBGColor,
       appBar: _buildAppBar(),
-      body: Container(
-        padding: EdgeInsets.symmetric(
-          horizontal: 20, 
-          vertical: 15
-          ),
-        child: Column(
-          children: [
-            searchBox(),
-            Expanded(
-              child: ListView(
-                children: [
-                  Container(
-                    margin: EdgeInsets.only(
-                      top: 50,
-                      bottom: 20,
-                    ),
-                    child: Text(
-                      'All Tasks',
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                  ),
-                  ToDoItem(), 
-                  ToDoItem(), 
-                  ToDoItem(), 
-                  ToDoItem(), 
-                  ToDoItem(),                  
-                ],
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 20, 
+              vertical: 15
               ),
-            )
-          ],
-        ),
+            child: Column(
+              children: [
+                searchBox(),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.only(
+                          top: 50,
+                          bottom: 20,
+                        ),
+                        child: Text(
+                          'All Tasks',
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+
+                      for ( ToDo todoo in todosList ) 
+                      ToDoItem(todo: todoo,),              
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    left: 20,
+                    right: 20,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: const [BoxShadow(
+                      color: Colors.grey,
+                      offset: Offset(0.0, 0.0),
+                      blurRadius: 10.0,
+                      spreadRadius: 0.0,
+                    ),],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Add a new todo item',
+                      border: InputBorder.none
+                    ),
+                  )
+                ), 
+              ),
+            ]),
+          )
+        ],
       ),
     );
   }
